f6ab389e2f0c1e952b5eb8419f8a1310c4ce6235
photodude <photodude@users.noreply.github.com>
2015-07-26 21:59:32 -0600

    Update InputFilter.php

--- src/InputFilter.php
+++ src/InputFilter.php
@@ -183,7 +183,7 @@ class InputFilter
 					preg_match('/-?[0-9]+/', (string) $source, $matches);
 				}
 
-				if (is_array($matches) && empty($matches[1]) && $matches[1] != 0)
+				if (is_array($matches) && empty($matches[1]))
 				{
 					$result = (int) $matches[0];
 				}
