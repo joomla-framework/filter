b4071b8706772faf7fabcc288203a5fc7332081d
David Jardin <d.jardin@djumla.de>
2024-07-08 09:48:03 +0200

    Fix case insensitvity

--- src/OutputFilter.php
+++ src/OutputFilter.php
@@ -262,7 +262,7 @@ class OutputFilter
      */
     public static function stripImages($string)
     {
-        return preg_replace('#(<[/]?img.*>)#U', '', $string);
+        return preg_replace('#(<[/]?img.*>)#Ui', '', $string);
     }
 
     /**
@@ -276,6 +276,6 @@ class OutputFilter
      */
     public static function stripIframes($string)
     {
-        return preg_replace('#(<[/]?iframe.*>)#U', '', $string);
+        return preg_replace('#(<[/]?iframe.*>)#Ui', '', $string);
     }
 }
