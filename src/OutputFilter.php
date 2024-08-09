5b4917e12ec20286fc82de4ceec3ba55f0995ddd
David Jardin <d.jardin@djumla.de>
2024-08-09 10:45:58 +0200

    Update src/OutputFilter.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- src/OutputFilter.php
+++ src/OutputFilter.php
@@ -272,7 +272,7 @@ class OutputFilter
 	 * @return  string  Cleaned string
 	 *
 	 * @since   1.0
- */
+	 */
 	public static function stripImages($string)
 	{
 		while (preg_match('#(<[/]?img.*>)#Ui', $string)) {
